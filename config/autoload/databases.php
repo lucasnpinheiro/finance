75939f23cbd5771ba1442c7285a5136512e768d0
lucasnpinheiro <lucasnpinheiro@gmail.com>
2024-10-29 15:11:19 -0300

    Creating configuration to connect to the database.

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -15,7 +15,7 @@ return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
         'host' => env('DB_HOST', 'localhost'),
-        'database' => env('DB_DATABASE', 'hyperf'),
+        'database' => env('DB_DATABASE', 'finance'),
         'port' => env('DB_PORT', 3306),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', ''),
