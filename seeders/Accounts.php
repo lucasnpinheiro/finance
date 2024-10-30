4081f3da10f61c3f164de0e590ca1b26e4300579
lucasnpinheiro <lucasnpinheiro@gmail.com>
2024-10-30 12:56:54 -0300

    Implementing fixed scenarios

--- seeders/Accounts.php
+++ seeders/Accounts.php
@@ -14,6 +14,14 @@ class Accounts extends Seeder
      */
     public function run()
     {
+        Account::create([
+            'id' => '7557f6da-61f7-4e9c-8479-88aa52ed2050',
+            'balance' => 99999,
+        ]);
+        Account::create([
+            'id' => '68c2c56d-8310-4628-b886-a82fccc289f5',
+            'balance' => 99999,
+        ]);
         for ($i = 0; $i < 10; $i++) {
             Account::create([
                 'id' => \Ramsey\Uuid\Uuid::uuid4()->toString(),
